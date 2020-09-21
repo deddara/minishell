b9682030900225f9adb693740c20347732691b5a
deddara <deddara@student.21-school.ru>
2020-09-21 23:10:39 +0300

    env added

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: deddara <deddara@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 00:51:31 by awerebea          #+#    #+#             */
-/*   Updated: 2020/09/21 23:01:10 by deddara          ###   ########.fr       */
+/*   Updated: 2020/09/21 23:09:06 by deddara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,6 @@ int				main(int argc, char **argv, char **envp)
 	f_data_init(&data);
 	if (!(data.envp = f_strarr_dup(envp)))
 		return (f_exit(&data, 1, "malloc error\n"));
-	f_pwd(1);
+	f_env(1, &data);
 	return (f_exit(&data, 0, ""));
 }
