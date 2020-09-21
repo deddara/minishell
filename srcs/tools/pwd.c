48933b6e4553969e7eda09fcf0e864ae9f35a8a8
deddara <deddara@student.21-school.ru>
2020-09-21 22:59:11 +0300

    fd added

--- srcs/tools/pwd.c
+++ srcs/tools/pwd.c
@@ -6,17 +6,17 @@
 /*   By: deddara <deddara@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/21 20:00:58 by deddara           #+#    #+#             */
-/*   Updated: 2020/09/21 20:09:27 by deddara          ###   ########.fr       */
+/*   Updated: 2020/09/21 22:58:44 by deddara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void f_pwd(void)
+void f_pwd(int fd)
 {
 	char *dir;
 	if (!(dir = getcwd(NULL, 0)))
 		return ;
-	write (1, dir, ft_strlen(dir));
-	write (1, "\n", 1);
+	write (fd, dir, ft_strlen(dir));
+	write (fd, "\n", 1);
 }
