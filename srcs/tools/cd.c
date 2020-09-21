b7627eed69b11039f086da671eaf8354771e7142
deddara <deddara@student.21-school.ru>
2020-09-21 21:30:18 +0300

    find element in env function added

--- srcs/tools/cd.c
+++ srcs/tools/cd.c
@@ -6,17 +6,46 @@
 /*   By: deddara <deddara@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/21 20:18:54 by deddara           #+#    #+#             */
-/*   Updated: 2020/09/21 21:13:44 by deddara          ###   ########.fr       */
+/*   Updated: 2020/09/21 21:29:41 by deddara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
+
+char	*f_env_find_elem(char **src_arr, char *str, char *endcmp)
+{
+	int			i;
+	int			j;
+	int			len;
+	char		*coincidence;
+
+	if (!str)
+		return (NULL);
+	len = ft_strlen(str);
+	i = -1;
+	j = 0;
+	while (src_arr[++i])
+	{
+		if ((coincidence = ft_strnstr(src_arr[i], str, len)) && \
+			coincidence == src_arr[i] && (ft_strchr(endcmp, src_arr[i][len]) \
+			&& !(!src_arr[i][len] && ft_strlen(endcmp))))
+		{
+			while (*coincidence != '=')
+				coincidence++;
+			coincidence++;
+			return (coincidence);
+		}
+	}
+	return (NULL);
+}
+
+
 int		f_cd(char *path, char **envp)
 {
 	if (!path)
 	{
-		chdir("~/");
+		f_env_find_elem(envp, "HOME", "=");
 		f_pwd();
 		return (0);
 	}
