49cb5e2a51c35d688f02cf0d77c6003b97115927
deddara <deddara@student.21-school.ru>
2020-09-21 20:10:09 +0300

    pwd function added

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: deddara <deddara@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 00:51:31 by awerebea          #+#    #+#             */
-/*   Updated: 2020/09/21 19:40:39 by deddara          ###   ########.fr       */
+/*   Updated: 2020/09/21 20:07:55 by deddara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ int				main(int argc, char **argv, char **envp)
 	f_data_init(&data);
 	if (!(data.envp = f_strarr_dup(envp)))
 		return (f_exit(&data, 1, "malloc error\n"));
+	f_pwd();
 	return (f_exit(&data, 0, ""));
 }
