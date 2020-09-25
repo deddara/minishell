2da0a044ecbac567e6bc6e739f5141a7ca3a647a
deddara <deddara@student.21-school.ru>
2020-09-25 19:45:55 +0300

    norme

--- srcs/tools/structer.c
+++ srcs/tools/structer.c
@@ -1,10 +1,10 @@
-
 #include "minishell.h"
 #include "structer.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "libft.h"
+
 static t_command	*create_command_lst(void)
 {
 	t_command *tmp;
@@ -20,12 +20,93 @@ static t_command	*create_command_lst(void)
 	return (tmp);
 }
 
+static int	pipe_handler(t_data *data, t_command *cmd_tmp, int i)
+{
+	if (ft_strncmp(data->inp_arr[i + 1], ">", 1) == 0)
+		cmd_tmp->redirect = 1;
+	else if (ft_strncmp(data->inp_arr[i + 1], ">>", 2) == 0)
+		cmd_tmp->redirect = 2;
+	else if (ft_strncmp(data->inp_arr[i + 1], "<", 1) == 0)
+		cmd_tmp->redirect = 3;
+	cmd_tmp->pipe = 1;
+	if (!(cmd_tmp->next = create_command_lst()))
+		return (1);
+	cmd_tmp = cmd_tmp->next;
+	return (0);
+}
+
+static int	valid_check(t_data *data, int i)
+{
+	if (data->inp_arr[0][0] == '|')
+	{
+		write (2, "syntax error near unexpected token `|'\n", 39);
+		return (258);
+	}
+	if (data->inp_arr[i][0] == '|' && !data->inp_arr[i + 1])
+	{
+		write(2, "undefined behavior: multiple lines\n", 35);
+		return (1);
+	}
+		if ((data->inp_arr[i][0] == '<' || data->inp_arr[i][0] == '>') \
+				&& !data->inp_arr[i + 1])
+	{
+		write(2, "syntax error near unexpected token `newline'\n", 45);
+		return (1);
+	}
+	return (0);
+}
+
+static int	redirect_handler(t_data *data, t_command *cmd_tmp, int i)
+{
+	if (data->inp_arr[i + 1][0] == '|' && (data->inp_arr[i][0] == '<' || \
+			!ft_strncmp(data->inp_arr[i], ">>", 2)))
+	{
+		write(2, "syntax error near unexpected token `newline'\n", 45);
+		return (258);
+	}
+	if (ft_strncmp(data->inp_arr[i], ">", 1) == 0)
+		cmd_tmp->redirect = 1;
+	else if (ft_strncmp(data->inp_arr[i], ">>", 2) == 0)
+		cmd_tmp->redirect = 2;
+	else if (ft_strncmp(data->inp_arr[i], "<", 1) == 0)
+		cmd_tmp->redirect = 3;
+	if (!(cmd_tmp->next = create_command_lst()))
+		return (1);
+	cmd_tmp = cmd_tmp->next;
+	return (0);
+}
+
+static int struct_handler(t_data *data, t_command *cmd_tmp, int i)
+{
+	int err_code;
+
+	if ((err_code = valid_check(data, i)))
+		return (err_code);
+	if (data->inp_arr[i][0] == '|')
+	{
+		if (pipe_handler(data, cmd_tmp, i))
+			return (1);
+	}
+	else if (data->inp_arr[i][0] == '>' || data->inp_arr[i][0] == '<' || \
+		!ft_strncmp(data->inp_arr[i], ">>", 2))
+	{
+		if ((err_code = redirect_handler(data, cmd_tmp, i)))
+			return (err_code);
+	}
+	else if (!(cmd_tmp->argv = f_strarr_add_elem(cmd_tmp->argv, data->inp_arr[i])))
+	{
+		write(2, "malloc error\n", 13);
+		return (1);
+	}
+	return (0);
+}
 
 int		structer(t_data *data)
 {
 	int			i;
 	t_command	*cmd;
 	t_command	*cmd_tmp;
+	int 		err_code;
 
 	if (!(cmd = create_command_lst()))
 		return (1);
@@ -33,72 +114,9 @@ int		structer(t_data *data)
 	i = 0;
 	while (data->inp_arr[i])
 	{
-		if (data->inp_arr[0][0] == '|')
-		{
-			write (2, "syntax error near unexpected token `|'\n", 39);
-			return (258);
-		}
-		if (data->inp_arr[i][0] == '|' && !data->inp_arr[i + 1])
-		{
-			write(2, "undefined behavior: multiple lines\n", 35);
-			return (1);
-		}
-		if ((data->inp_arr[i][0] == '<' || data->inp_arr[i][0] == '>') \
-		&& !data->inp_arr[i + 1])
-		{
-			write(2, "syntax error near unexpected token `newline'\n", 45);
-			return (1);
-		}
-		if (data->inp_arr[i][0] == '|')
-		{
-			if (ft_strncmp(data->inp_arr[i + 1], ">", 1) == 0)
-				cmd_tmp->redirect = 1;
-			else if (ft_strncmp(data->inp_arr[i + 1], ">>", 2) == 0)
-				cmd_tmp->redirect = 2;
-			else if (ft_strncmp(data->inp_arr[i + 1], "<", 1) == 0)
-				cmd_tmp->redirect = 3;
-			cmd_tmp->pipe = 1;
-			cmd_tmp->next = create_command_lst();
-			cmd_tmp = cmd_tmp->next;
-			i++;
-			continue;
-		}
-		else if (data->inp_arr[i][0] == '>' || data->inp_arr[i][0] == '<' || \
-		!ft_strncmp(data->inp_arr[i], ">>", 2))
-		{
-			if (data->inp_arr[i + 1][0] == '|' && (data->inp_arr[i][0] == '<' || \
-			!ft_strncmp(data->inp_arr[i], ">>", 2)))
-			{
-				write(2, "syntax error near unexpected token `newline'\n", 45);
-				return (258);
-			}
-			if (ft_strncmp(data->inp_arr[i], ">", 1) == 0)
-				cmd_tmp->redirect = 1;
-			else if (ft_strncmp(data->inp_arr[i], ">>", 2) == 0)
-				cmd_tmp->redirect = 2;
-			else if (ft_strncmp(data->inp_arr[i], "<", 1) == 0)
-				cmd_tmp->redirect = 3;
-			cmd_tmp->next = create_command_lst();
-			cmd_tmp = cmd_tmp->next;
-			i++;
-			continue;
-		}
-		if (!(cmd_tmp->argv = f_strarr_add_elem(cmd_tmp->argv, data->inp_arr[i])))
-		{
-			write(2, "malloc error\n", 13);
-			return (1);
-		}
+		if ((err_code = struct_handler(data, cmd_tmp, i)))
+			return (err_code);
 		i++;
 	}
-//	while (cmd)
-//	{
-//		int j = 0;
-//		while (cmd->argv[j]) {
-//			ft_putstr_fd(cmd->argv[j], 1);
-//			ft_putstr_fd("\n", 1);
-//			j++;
-//		}
-//		cmd = cmd->next;
-//	}
 	return (0);
 }
