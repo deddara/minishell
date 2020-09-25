7e2c02324339934719aafc20a3976185fc7187ac
deddara <deddara@student.21-school.ru>
2020-09-25 16:27:29 +0300

    interm

--- srcs/tools/structer.c
+++ srcs/tools/structer.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "libft.h"
 static t_command	*create_command_lst(void)
 {
 	t_command *tmp;
@@ -12,7 +13,8 @@ static t_command	*create_command_lst(void)
 		return (NULL);
 	tmp->pipe = 0;
 	tmp->redirect = 0;
-	tmp->argv = NULL;
+	tmp->argv = malloc(sizeof(char) * 2);
+	tmp->argv[1] = 0;
 	tmp->next = NULL;
 	return (tmp);
 }
@@ -37,9 +39,22 @@ int		structer(t_data *data)
 		}
 		if (data->inp_arr[i][0] == '|')
 		{
+			if (ft_strncmp(data->inp_arr[i + 1], ">", 1) == 0)
+				cmd_tmp->pipe = 1;
+			else if (ft_strncmp(data->inp_arr[i + 1], ">>", 2) == 0)
+				cmd_tmp->pipe = 2;
+			else if (ft_strncmp(data->inp_arr[i + 1], "<", 1) == 0)
+				cmd_tmp->pipe = 3;
 			cmd_tmp->pipe = 1;
 			cmd_tmp->next = create_command_lst();
 			cmd_tmp = cmd_tmp->next;
+			i++;
+			continue;
+		}
+		if (!(cmd_tmp->argv = f_strarr_add_elem(cmd_tmp->argv, data->inp_arr[i])))
+		{
+			write(2, "malloc error\n", 13);
+			return (1);
 		}
 		i++;
 	}
