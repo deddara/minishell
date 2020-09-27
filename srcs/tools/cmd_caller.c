612bb3351f1d8f0842d2782835e05bc766e28627
deddara <amirhan16616@gmail.com>
2020-09-27 22:20:46 +0300

    pipes handled

--- srcs/tools/cmd_caller.c
+++ srcs/tools/cmd_caller.c
@@ -71,19 +71,51 @@ static int execute_one(t_command *cmd, t_data *data)
     return (0);
 }
 
+static void process_handler(t_command *cmd, t_data *data, int kind)
+{
+    dup2(data->fd[kind], kind);
+    close(data->fd[0]);
+    close(data->fd[1]);
+    if (kind == 1)
+        cmd->pipe = 0;
+    cmd_caller(data, cmd);
+    exit (0);
+}
+
+static int execute(t_command *cmd, t_data *data)
+{
+    pid_t pid_1;
+    pid_t pid_2;
+    int     status_1;
+    int     status_2;
+
+    if (pipe(data->fd) == 1)
+        return (1);
+    if ((pid_1 = fork()) < 0)
+        return (1);
+    if (pid_1 == 0)
+        process_handler(cmd, data, 1);
+    if ((pid_2 = fork()) < 0)
+        return (1);
+    if (pid_2 == 0)
+        process_handler(cmd->next, data, 0);
+    close(data->fd[0]);
+    close(data->fd[1]);
+    waitpid(pid_1, &status_1, 0);
+    waitpid(pid_2, &status_2, 0);
+    return (1);
+}
+
 int			cmd_caller(t_data *data, t_command *cmd)
 {
-//	t_command *prev;
 	data->counter = 0;
-//	prev = cmd;
-	if (cmd->next == NULL)
+	if (cmd->pipe == 0)
 	    return (execute_one(cmd, data));
-//	while (cmd)
-//	{
-//		if (execute(cmd, prev, data))
-//			return (1);
-//		prev = cmd;
-//		cmd = cmd->next;
-//	}
+	while (cmd)
+	{
+		if (execute(cmd, data))
+			return (0);
+		cmd = cmd->next;
+	}
 	return (0);
 }
