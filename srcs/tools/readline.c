0ec6d6004240d48d425c2ec824a32cad359386ba
awerebea <awerebea.21@gmail.com>
2020-10-05 22:13:43 +0300

    intermediate

--- srcs/tools/readline.c
+++ srcs/tools/readline.c
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 15:18:25 by awerebea          #+#    #+#             */
-/*   Updated: 2020/10/05 21:53:02 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/10/05 22:13:26 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,78 +15,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/* static void		f_sigquit_case(char **input, char *line)                    */
-/* {                                                                        */
-/*     if (*input)                                                          */
-/*         free(*input);                                                    */
-/*     *input = NULL;                                                       */
-/*     if (line)                                                            */
-/*     {                                                                    */
-/*         free(line);                                                      */
-/*         line = ft_strdup("");                                            */
-/*     }                                                                    */
-/*     g_sigquit = 0;                                                       */
-/* }                                                                        */
-
-/* static int		f_clear_n_print_exit(char *line)                             */
-/* {                                                                        */
-/*     [> (void)line; <]                                                    */
-/*     if (line)                                                            */
-/*         free(line);                                                      */
-/*     ft_putstr_fd("exit\n", 1);                                           */
-/*     return (4);                                                          */
-/* }                                                                        */
-
-/* static int		f_other_cases(char **input, char **line, char *buf, int res) */
-/* {                                                                        */
-/*     char		*tmp;                                                          */
-
-/*     if (!res && !ft_strlen(*line))                                       */
-/*         return (f_clear_n_print_exit(*line));                            */
-/*     if (!res)                                                            */
-/*         ft_putstr_fd("  \b\b", 1);                                       */
-/*     if (buf[0] != '\n')                                                  */
-/*     {                                                                    */
-/*         if (res)                                                         */
-/*         {                                                                */
-/*             tmp = *line;                                                 */
-/*             if (!(*line = ft_strjoin(*line, buf)))                       */
-/*                 return (2);                                              */
-/*             free(tmp);                                                   */
-/*         }                                                                */
-/*     }                                                                    */
-/*     else                                                                 */
-/*     {                                                                    */
-/*         *input = *line;                                                  */
-/*         return (1);                                                      */
-/*     }                                                                    */
-/*     return (0);                                                          */
-/* }                                                                        */
-
-/* int				f_readline(char **input)                                          */
-/* {                                                                        */
-/*     char		*line;                                                         */
-/*     char		buf[2];                                                        */
-/*     int			res;                                                           */
-/*     int			result;                                                        */
-
-/*     line = ft_strdup("");                                                */
-/*     ft_bzero(buf, 2);                                                    */
-/*     while (1)                                                            */
-/*     {                                                                    */
-/*         if ((res = read(0, buf, 1)) == -1)                               */
-/*             return (2);                                                  */
-/*         if (g_sigquit)                                                   */
-/*             f_sigquit_case(input, line);                                 */
-/*         if ((result = f_other_cases(input, &line, buf, res)))            */
-/*             return (result - 1);                                         */
-/*     }                                                                    */
-/* }                                                                        */
-
+static void sigquit_handle(char **input, char **line)
+{
+	if (*input)
+		free(*input);
+	*input = NULL;
+	if (*line)
+	{
+		free(*line);
+		*line = ft_strdup("");
+	}
+	g_sigquit = 0;
+}
+static int readline_handler(char **line, char buf[2], char **input, int res)
+{
+	char		*tmp;
+	if (!res)
+		ft_putstr_fd("  \b\b", 1);
+	if (buf[0] != '\n')
+	{
+		if (res)
+		{
+		tmp = *line;
+		if (!(*line = ft_strjoin(*line, buf)))
+			return (2);
+		free(tmp);
+		}
+	}
+	else
+	{
+		*input = *line;
+		return (1);
+	}
+	return (0);
+}
 int				f_readline(char **input)
 {
 	char		*line;
-	char		*tmp;
 	char		buf[2];
 	int			res;
 	line = ft_strdup("");
@@ -96,42 +61,17 @@ int				f_readline(char **input)
 		if ((res = read(0, buf, 1)) == -1)
 			return (2);
 		if (g_sigquit)
-		{
-			if (*input)
-				free(*input);
-			*input = NULL;
-			if (line)
-			{
-				free(line);
-				line = ft_strdup("");
-			}
-			g_sigquit = 0;
-		}
+		sigquit_handle(input, &line);
 		if (!res && !ft_strlen(line))
 		{
-			if (line)
-				free(line);
-			ft_putstr_fd("exit\n", 1);
+		if (line)
+			free(line);
+		ft_putstr_fd("exit\n", 1);
 			return (3);
 		}
 		if (!res)
-		{
-			ft_putstr_fd("  \b\b", 1);
-		}
-		if (buf[0] != '\n')
-		{
-			if (res)
-			{
-				tmp = line;
-				if (!(line = ft_strjoin(line, buf)))
-					return (1);
-				free(tmp);
-			}
-		}
-		else
-		{
-			*input = line;
-			return (0);
-		}
+		ft_putstr_fd("  \b\b", 1);
+		if ((res = readline_handler(&line, buf, input, res)))
+			return (res - 1);
 	}
 }
